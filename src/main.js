f75d3e8521e66f5fb5beef4c709850e21d489459
Havrilenko Olexiy <hristinson@gmail.com>
2022-08-21 23:41:29 +0300

    Try tu create navbar menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import PrimeVue from "primevue/config";
+import Menubar from "primevue/menubar";
+
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true });
+app.component(`Menubar`, Menubar);
+app.mount("#app");
