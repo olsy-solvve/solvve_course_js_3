653525e1933a089d70ba5f0270670fde208c8a81
Havrilenko Olexiy <hristinson@gmail.com>
2022-08-25 20:38:39 +0300

    Lint test passed

--- src/main.js
+++ src/main.js
@@ -6,18 +6,18 @@ import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import PrimeVue from "primevue/config";
 import Menubar from "primevue/menubar";
-import DataView from 'primevue/dataview';
-import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions';
-import Panel from 'primevue/panel';
-import Paginator from 'primevue/paginator';
+import DataView from "primevue/dataview";
+import DataViewLayoutOptions from "primevue/dataviewlayoutoptions";
+import PanelPanel from "primevue/panel";
+import PaginatorPaginator from "primevue/paginator";
 
 const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
 app.component(`Menubar`, Menubar);
 app.component(`DataView`, DataView);
 app.component("DataViewLayoutOptions", DataViewLayoutOptions);
-app.component("Panel", Panel);
-app.component("Paginator", Paginator);
+app.component("PanelPanel", PanelPanel);
+app.component("PaginatorPaginator", PaginatorPaginator);
 
 import Router from "./plugins/router.js";
 import "./assets/main.css";
