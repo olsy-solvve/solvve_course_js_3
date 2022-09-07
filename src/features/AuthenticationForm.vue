1c29c6be467cdc66fa218f051f3003afed617b20
KarunaGray <sergeykaruna2@gmail.com>
2022-09-07 03:23:01 +0300

    Adding auth form (for view only)

--- src/features/AuthenticationForm.vue
+++ src/features/AuthenticationForm.vue
@@ -1,7 +1,39 @@
 <script></script>
 
 <template>
-  <main>Authentication Form</main>
+  <div class="flex mx-auto justify-content-center mt-5">
+    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6 my-auto">
+      <div class="text-center mb-5">
+        <div class="text-900 text-5xl font-bold">Welcome Back</div>
+        <div class="text-500 text-2xl font-normal mb-2">May the Force be with you</div>>
+        <span class="text-600 font-medium line-height-3"
+          >Don't have an account?</span
+        >
+        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer"
+          >Create today!</a
+        >
+      </div>
+
+      <div>
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <InputText id="email1" type="text" class="w-full mb-3" />
+
+        <label for="password1" class="block text-900 font-medium mb-2"
+          >Password</label
+        >
+        <InputText id="password1" type="password" class="w-full mb-3" />
+
+        <div class="flex align-items-center justify-content-between mb-6">
+          <a
+            class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"
+            >Forgot password?</a
+          >
+        </div>
+
+        <Button label="Sign In" icon="pi pi-user" class="w-full"></Button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ import Panel from "primevue/panel";
 import Paginator from "primevue/paginator";
 import PrimeFlex from "primeflex/primeflex.css";
 import Carousel from "primevue/carousel";
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
+import Button from 'primevue/button';
 
 const app = createApp(App);
 app.use(PrimeVue, { ripple: true });
@@ -26,6 +29,9 @@ app.component("PanelVue", Panel);
 app.component("PaginatorVue", Paginator);
 app.component("PrimeFlex", PrimeFlex);
 app.component("CarouselVue", Carousel);
+app.component("InputText",InputText);
+app.component("Checkbox",Checkbox);
+app.component("Button",Button);
 
 import Router from "./plugins/router.js";
 import "./assets/main.css";
