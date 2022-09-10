6bf9126e75d583e42a0a213c6f46cc651ace82ff
Oleksiy Husar <aleksey.gusar.2@gmail.com>
2022-09-10 22:47:51 +0300

    add starships

--- frontend/src/features/MyStarships.vue
+++ frontend/src/features/MyStarships.vue
@@ -5,6 +5,7 @@
       <template #grid="slotProps">
         <div style="padding: 0.5em 0" class="col-12 md:col-4 lg:col-3 xs:col-6">
           <PanelVue :header="slotProps.data.name" style="text-align: center">
+            <img :src="'/src/assets/images.jpg/starships/' + generateRandImg() + '.jpg'" :alt="slotProps.data.brand"/>
             <div class="people-detail">
               model: {{ slotProps.data.model }}<br />
               manufacturer: {{ slotProps.data.manufacturer }}<br />
@@ -59,6 +60,9 @@ export default {
       });
   },
   methods: {
+     generateRandImg(){
+      return Math.floor(Math.random() * 6);
+    },
     onPage(event) {
       this.preloader = true;
       this.items = [];
